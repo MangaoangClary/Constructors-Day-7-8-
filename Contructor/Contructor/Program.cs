7eb7ab7e3199379bf7f1a0c3dc6b3cbcd34494c0
MangaoangClary <116094433+MangaoangClary@users.noreply.github.com>
2022-10-21 11:17:10 +0800

    Add files via upload

---
+++ Contructor/Contructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Angel Locsin";
+            int age = 30;
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("You are " + age +  " years old");
+        }
+    }
+}
